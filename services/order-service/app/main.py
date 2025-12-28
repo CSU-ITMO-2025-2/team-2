5df7a2f64c1c9744710608abeb6c10251a94a49a
danilsocol <danilsocol-2002@mail.ru>
2025-12-28 18:20:46 +0500

    Делаем вид что у нас обновились сервисы

--- services/order-service/app/main.py
+++ services/order-service/app/main.py
@@ -130,6 +130,5 @@ async def shutdown_event():
 
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run("app.main:app", host="0.0.0.0", port=int(os.getenv("PORT", "8000")), reload=True)
 
